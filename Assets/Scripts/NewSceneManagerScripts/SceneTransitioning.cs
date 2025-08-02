4d5a052687fa36f7225c9f0b1d81c46c4c66305b
SahirChowdhury2174 <103613989+SahirChowdhury2174@users.noreply.github.com>
2025-08-02 19:45:39 -0400

    Miscellaneous Things

--- Assets/Scripts/NewSceneManagerScripts/SceneTransitioning.cs
+++ Assets/Scripts/NewSceneManagerScripts/SceneTransitioning.cs
@@ -3,8 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class SceneTransitioning : MonoBehaviour
 {
+
+    public GameObject thisGameObject;
     [SerializeField] public string[] levels;
-    
+
+
     public void onButtonClick()
     {
         SceneManager.LoadScene(levels[0]);
