c296f0c0a06e574ece261a77755a1073b6e10f2a
SahirChowdhury2174 <103613989+SahirChowdhury2174@users.noreply.github.com>
2025-08-03 00:26:59 -0400

    Fade in for pre-view screen and curtain fade complete

--- Assets/Scripts/NewSceneManagerScripts/Curtainscript.cs
+++ Assets/Scripts/NewSceneManagerScripts/Curtainscript.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEditor.SearchService;
-
+using UnityEngine.Rendering;
+using System.Collections;
+using Unity.VisualScripting;
 public class Curtainscript : MonoBehaviour
 {
+    private bool waitTimeFin = false;
+    public float curtainSpeed; 
     public TextMeshProUGUI World;
     public TextMeshProUGUI Lives;
+    public GameObject player;
+    public RectTransform rectTransform;
     private string[] levels = { "SampleScene", "TickyTwo", "TickyAndTocky", "Ticky4", "Ticky5", "TickSix", "TickyRem" };
     
     private void Start()
@@ -21,6 +27,31 @@ public class Curtainscript : MonoBehaviour
             sceneNum++;
         }
 
-        World.text = "World-" + (sceneNum + 1); 
+        World.text = "World-" + (sceneNum + 1);
+        Lives.text = "Lives - " + player.GetComponent<PlayerHealth>().currentHealth.ToString();
+    }
+
+    private void FixedUpdate()
+    {
+        if (waitTimeFin)
+        {
+            rectTransform.transform.position += new Vector3(0, curtainSpeed, 0);
+        }
+        else
+        {
+            StartCoroutine(CurtainScroll(5f));
+        }
+    }
+    IEnumerator CurtainScroll(float delay)
+    {
+
+        yield return new WaitForSeconds(delay);
+
+        updateBoolFunction();
+    }
+
+    private void updateBoolFunction()
+    {
+        waitTimeFin = true;
     }
 }
---
+++ Assets/Scripts/NewSceneManagerScripts/PreMenuScreen.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PreMenuScreen : MonoBehaviour
+{
+    [SerializeField] private Image image; // Assign in Inspector if needed
+    private bool waitTimeFin;
+    public float fadeDuration = 1f;
+    private bool isFading = false;
+    public float blackScreenStayTime;
+
+    private List<Image> imagesToFade;
+    private List<TextMeshProUGUI> textsToFade;
+
+    private void Awake()
+    {
+        // Collect all Image components in this object and its children
+        imagesToFade = new List<Image>(GetComponentsInChildren<Image>(true));
+        // Collect all TextMeshProUGUI components in this object and its children
+        textsToFade = new List<TextMeshProUGUI>(GetComponentsInChildren<TextMeshProUGUI>(true));
+    }
+
+    private void FixedUpdate()
+    {
+        if (waitTimeFin && !isFading)
+        {
+            StartCoroutine(FadeOutUI());
+            isFading = true;
+        }
+        else if (!waitTimeFin)
+        {
+            StartCoroutine(CurtainScroll(blackScreenStayTime));
+        }
+    }
+
+    IEnumerator CurtainScroll(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        updateBoolFunction();
+    }
+
+    private void updateBoolFunction()
+    {
+        waitTimeFin = true;
+    }
+
+    private IEnumerator FadeOutUI()
+    {
+        float elapsed = 0f;
+        // Store original colors
+        List<Color> originalImageColors = new List<Color>();
+        foreach (var img in imagesToFade)
+            originalImageColors.Add(img.color);
+
+        List<Color> originalTextColors = new List<Color>();
+        foreach (var txt in textsToFade)
+            originalTextColors.Add(txt.color);
+
+        while (elapsed < fadeDuration)
+        {
+            elapsed += Time.deltaTime;
+            float alpha = Mathf.Lerp(1f, 0f, elapsed / fadeDuration);
+
+            for (int i = 0; i < imagesToFade.Count; i++)
+            {
+                Color c = originalImageColors[i];
+                c.a = alpha;
+                imagesToFade[i].color = c;
+            }
+            for (int i = 0; i < textsToFade.Count; i++)
+            {
+                Color c = originalTextColors[i];
+                c.a = alpha;
+                textsToFade[i].color = c;
+            }
+            yield return null;
+        }
+        // Ensure alpha is set to 0 at the end
+        for (int i = 0; i < imagesToFade.Count; i++)
+        {
+            Color c = originalImageColors[i];
+            c.a = 0f;
+            imagesToFade[i].color = c;
+        }
+        for (int i = 0; i < textsToFade.Count; i++)
+        {
+            Color c = originalTextColors[i];
+            c.a = 0f;
+            textsToFade[i].color = c;
+        }
+    }
+}
